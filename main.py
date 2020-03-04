77bcdcd45e6050d9957600f44b38a2b4b75df851
Md. Mashur Shalehin Sakib <mashursakib1920@gmail.com>
2020-03-04 01:11:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# =+
+def checkMagazine(m,n,b):
+  print(n)
+  print(b)
+
+  return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    test = int(input())
+
+    for i in range(test):
+      number = int(input())
+      necklace = sorted(list(int(i) for i in input().split()))
+      bracelet = sorted(list(int(i) for i in input().split()))
+      checkMagazine(number,necklace, bracelet)
+
+
+
+
+    # 
